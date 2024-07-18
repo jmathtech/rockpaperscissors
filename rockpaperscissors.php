769f983ad6776e080d2d02bffb4b85b6b30aa51b
Jamil Matheny <admin@jamilimages.com>
2024-07-18 14:31:02 -0400

    Update rockpaperscissors.php

--- rockpaperscissors.php
+++ rockpaperscissors.php
@@ -1,74 +1,68 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Rock, Paper, Scissors | SHOOT!</title>
-            <link rel="stylesheet" href="css/rpsstyle.css">
-    </head>
-
-    <body>
-
-        <table border="1px">
-            <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="POST">
-                <tr>
-                    <td><h2>Rock, Paper, Scissors</h2>
+<head>
+    <title>Rock, Paper, Scissors | SHOOT!</title>
+    <link rel="stylesheet" href="css/rpsstyle.css">
+</head>
+<body>
+    <table border="1px">
+        <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+            <tr>
+                <td>
+                    <h2>Rock, Paper, Scissors</h2>
                     <h4>by Jamil Matheny</h4>
-                    </td>
-                </tr>
-                <tr>
-                    <td>Choose one of the three options:
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <input type="radio" id="rock" name="user_choice" value="Rock" />
-                        <label for="rock">Rock</label><br />
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <input type="radio" id="paper" name="user_choice" value="Paper" />
-                        <label for="paper">Paper</label><br />
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <input type="radio" id="scissors" name="user_choice" value="Scissors" />
-                        <label for="scissors">Scissors</label><br />
-                    </td>
-                </tr>
-                <tr>
-                    <td colspan="5"><input type="submit" value="Submit" />
-                    </td>
-                </tr> 
-                <tr>
-                    <td colspan="5">
-                        <?php 
+                </td>
+            </tr>
+            <tr>
+                <td>Choose one of the three options:</td>
+            </tr>
+            <tr>
+                <td>
+                    <input type="radio" id="rock" name="user_choice" value="Rock" />
+                    <label for="rock">Rock</label><br />
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <input type="radio" id="paper" name="user_choice" value="Paper" />
+                    <label for="paper">Paper</label><br />
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <input type="radio" id="scissors" name="user_choice" value="Scissors" />
+                    <label for="scissors">Scissors</label><br />
+                </td>
+            </tr>
+            <tr>
+                <td colspan="5"><input type="submit" value="Submit" /></td>
+            </tr>
+            <tr>
+                <td colspan="5">
+                    <?php
+                    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                        $user_choice = $_POST['user_choice'];
+                        $choices = array("Rock", "Paper", "Scissors");
+                        $comp_choice = $choices[rand(0, 2)];
 
-                        if($_SERVER["REQUEST_METHOD"] == "POST")    // data from form being stored in variable
-                            {   
-                                
-                                $user_choice = $_POST['user_choice'];   // user_choice is the name of the variable from the form.
-                                $choose = array("Rock", "Paper", "Scissors");   // Using the array to make a choice for the player and the computer.
-                                $choice = rand(0, 2);   // Random selection.
-                                $comp = $choose[$choice];   // What the computer will choose versus the player.
+                        if ($user_choice == $comp_choice) {
+                            $result = "It's a tie!";
+                        } elseif (
+                            ($user_choice == "Rock" && $comp_choice == "Scissors") ||
+                            ($user_choice == "Paper" && $comp_choice == "Rock") ||
+                            ($user_choice == "Scissors" && $comp_choice == "Paper")
+                        ) {
+                            $result = "YOU WIN!";
+                        } else {
+                            $result = "YOU LOSE!";
+                        }
 
-                            if($choice == $comp)    // If the computer picks the right choice; the computer will win and vice versa.
-                                        {
-                                            echo 'Player: '.$user_choice.' <br><br> CPU: '.$comp.' <br><br> Result: <b>YOU WIN!</b>';
-                                        }
-                                    else
-                                        {
-                                            echo 'Player: '.$user_choice.' <br><br> CPU: '.$comp.' <br><br> Result: <b>YOU LOSE!</b>';
-                                        }
-                                
-                            }
-                        ?> 
-                    </td>
-                </tr>
-            </form>
-        </table>
-
-    </body>
-
-    
+                        echo 'Player: ' . $user_choice . ' <br><br> CPU: ' . $comp_choice . ' <br><br> Result: <b>' . $result . '</b>';
+                    }
+                    ?>
+                </td>
+            </tr>
+        </form>
+    </table>
+</body>
 </html>
